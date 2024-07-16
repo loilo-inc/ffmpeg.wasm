45d5204f205e670171bab5fcaed97b7ac2d1bd40
kbinani <kbinani.bt@gmail.com>
2024-07-16 22:34:03 +0900

    Update upstream repository and version

--- build/ffmpeg-wasm.sh
+++ build/ffmpeg-wasm.sh
@@ -17,7 +17,6 @@ CONF_FLAGS=(
   -Llibavfilter 
   -Llibavformat 
   -Llibavutil 
-  # -Llibpostproc 
   -Llibswresample 
   -Llibswscale 
   -lavcodec 
@@ -25,7 +24,6 @@ CONF_FLAGS=(
   -lavfilter 
   -lavformat 
   -lavutil 
-  # -lpostproc 
   -lswresample 
   -lswscale 
   -Wno-deprecated-declarations 
